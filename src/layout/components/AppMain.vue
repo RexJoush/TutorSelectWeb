c4a4294a499cb854fa1058220c5f89a1e3bcebc4
Joush <37282701+RexJoush@users.noreply.github.com>
2021-08-08 20:33:49 +0800

    2021.08.08 Init

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,51 @@
+<!--
+ * @Description: your project
+ * @version: 1.0
+ * @Author: Rex Joush
+ * @Date: 2021-08-08 17:21:01
+ * @LastEditors: Rex Joush
+ * @LastEditTime: 2021-08-08 19:58:09
+-->
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <router-view :key="key" />
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain',
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  top: 115px;
+  z-index: 9;
+  padding-bottom: 150px;
+  position: relative;
+  overflow: hidden;
+}
+.fixed-header+.app-main {
+  padding-top: 50px;
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
---
+++ src/settings.js
@@ -0,0 +1,24 @@
+/*
+ * @Description: your project
+ * @version: 1.0
+ * @Author: Rex Joush
+ * @Date: 2021-08-08 17:21:01
+ * @LastEditors: Rex Joush
+ * @LastEditTime: 2021-08-08 20:28:32
+ */
+module.exports = {
+
+  title: '导师遴选系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
