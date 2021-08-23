145a83e884999f9741a0c3ff3a5732603c17eec0
Joush <37282701+RexJoush@users.noreply.github.com>
2021-08-23 21:48:14 +0800

    2021.08.23

--- src/api/departmentSecretary/secretaryFirst.js
+++ src/api/departmentSecretary/secretaryFirst.js
@@ -1,28 +1,26 @@
-import { param } from "@/utils";
-import request from "@/utils/request";
+import request from '@/utils/request'
 
-//初始化申请的所有类别（下拉框）
+// 初始化申请的所有类别（下拉框）
 export function getApplyType() {
   return request({
-    url: "/apply-type/getApplyType",
-    method: "get"
-  });
+    url: '/apply-type/getApplyType',
+    method: 'get'
+  })
 }
-//查询数据
+// 查询数据
 export function checkDate(data) {
   return request({
-    url: "/tutor-inspect/getAll",
-    method: "get",
-    params:data
-  });
+    url: '/tutor-inspect/admin/getAll',
+    method: 'get',
+    params: data
+  })
 }
-//更新操作
+// 更新操作
 export function updateStatus(data) {
   return request({
-    url: "/update-status/update",
-    method: "post",
+    url: '/update-status/update',
+    method: 'post',
     data: data
-  });
+  })
 }
-
 
