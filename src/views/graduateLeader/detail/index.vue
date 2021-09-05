3ad5e4b0f473e0d3ad231d56426ed2bd107c3f76
Anna667 <632177309@qq.com>
2021-09-05 15:52:53 +0800

    Anna 2021.09.07 15:52

---
+++ src/views/graduateLeader/detail/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: your project
+ * @version: 1.0
+ * @Author: Anna
+ * @Date: 2021-09-05 15:42:59
+ * @LastEditors: Anna
+ * @LastEditTime: 2021-09-05 15:47:44
+-->
+<template>
+  <div>研究生院领导基本信息</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
