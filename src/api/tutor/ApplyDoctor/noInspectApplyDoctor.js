46b44a9bea355a4bf2262f519c946b2922a73bca
xiao-jin-gang <71590074+xiao-jin-gang@users.noreply.github.com>
2021-09-08 21:47:01 +0800

    博士免审完善

---
+++ src/api/tutor/ApplyDoctor/noInspectApplyDoctor.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+import { baseUrl } from '@/api/url'
+
+//博士免审第一页提交
+export function submitFirstPage(data,applyCondition,applyId){
+    return request({
+        url:`${baseUrl}/tutor/noInspect/saveFirstPage/${applyCondition}/${applyId}`,
+        method: 'post',
+        data : data
+    })
+}
